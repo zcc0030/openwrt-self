bd4a5c1280ea97daa0a14ba4c5d23aa77e7ab9f1
zcc0030 <50785866+zcc0030@users.noreply.github.com>
2021-04-07 20:56:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,3 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/jerrykuku/lua-maxminddb.git ./package/lean/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git ./package/lean/luci-app-vssr
